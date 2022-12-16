c4840d3f63c37be138468c062a54601680f19195
rciochin <ciochina7@gmail.com>
2022-12-16 12:01:27 +0200

    1st commit

---
+++ src/main/java/com/example/springkafkawikimediastream/SpringKafkaWikimediaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkawikimediastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaWikimediaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaWikimediaStreamApplication.class, args);
+	}
+
+}
